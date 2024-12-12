44e703a8d92197620a90467cde0b905302873495
Javier Pedrera <javier@cyber-duck.co.uk>
2024-12-12 21:22:40 +0100

    Add seeder with proper dummy user with token included

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,22 +2,51 @@
 
 namespace Database\Seeders;
 
+use App\Models\Event;
+use App\Models\Reservation;
+use App\Models\Review;
 use App\Models\User;
-// use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
 {
-    /**
-     * Seed the application's database.
-     */
     public function run(): void
     {
-        // User::factory(10)->create();
-
-        User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
+        $user = User::factory()->create([
+            'name' => 'Digital Nomad',
+            'email' => 'nomaddeveloper@mydigitalnomads.co.uk',
+            'password' => bcrypt('secret'),
         ]);
+
+        $this->addCustomToken($user);
+
+        $user->events()->saveMany(
+            Event::factory()->count(5)->make()
+        )->each(function (Event $event) use ($user) {
+            $this->addReviewForEvent($event, $user);
+            $this->addReservationForEvent($event, $user);
+        });
+    }
+
+    private function addReviewForEvent(Event $event, User $user): void
+    {
+        $event->reviews()->save(
+            Review::factory()->make(['user_id' => $user->id])
+        );
+    }
+
+    private function addReservationForEvent(Event $event, User $user): void
+    {
+        $user->reservations()->save(
+            Reservation::factory()->make(['event_id' => $event->id])
+        );
+    }
+
+    protected function addCustomToken(User $user): void
+    {
+        $user->createToken('Seeded Token for Postman');
+        $token = $user->tokens()->first();
+        $token->token = 'e080760ea94879687925969f09899e0c35fc214038dfa1c058bc8a13ba61c89e';
+        $token->save();
     }
 }
